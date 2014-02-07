c6882b104bcd58b5f17a02c833a803083a6014ca
Zhou Yuchao <513629287@qq.com>
2014-02-07 15:35:21 -0600

    finish extract scores of battle

---
+++ beautifulsoup.py
@@ -0,0 +1,115 @@
+import urllib2;
+import re;
+import csv;
+from bs4 import BeautifulSoup
+
+
+def existDate(dateList, newDate):
+    result = "true";
+    for i in range(len(dateList)):
+        #print "list: ", dateList[i];
+        #print "new:  ", newDate;
+        if cmp(newDate, dateList[i]) == 0:
+            result = "true";
+            break;
+        else:
+            result = "false"; 
+    #print "result: ", result;
+    return result;
+
+def extractDate(htmlTable):
+    dateLine = htmlTable.find_all("tr", "stathead");
+    return dateLine[0].string;
+
+def extractHome(htmlTable):
+    home = [];
+    dateLine = htmlTable.find_all("td", align="right");
+    #print dateLine;
+    for a in dateLine:
+        homeLine = a.find_all("a");
+        if (homeLine):
+            home.append(homeLine[0].string);
+            #print homeLine[0].string;
+    return home;
+
+def extractScore(htmlTable):
+    scores = [];
+    scoreLine = htmlTable.find_all("td", align="center");
+    #print scoreLine;
+    for a in scoreLine:
+        score = a.find_all("a");
+        if (score):
+            scores.append(score[0].string);
+    return scores;
+    
+def extractLinks(htmlTable):
+    links = [];
+    linksLine = htmlTable.find_all("td", align="center");
+    #print linksLine;
+    for a in linksLine:
+        link = a.find_all("a");
+        if (link):
+            links.append(link[0].get('href'));
+    return links;
+
+def extractAway(htmlTable):
+    aways = [];
+    awaysLine = htmlTable.find_all("td", align="left");
+    #print awaysLine;
+    for a in awaysLine:
+        away = a.find_all("a");
+        if (away):
+            aways.append(away[0].string);
+    return aways;
+
+def writeCSV_init():
+    fileWriter = csv.writer(open('score.csv', 'a'));
+    fileWriter.writerow(['Home', 'Score', 'Away']);
+
+def writeCSV(bDate, bHome, bScore, bAway):
+    fileWriter = csv.writer(open('score.csv', 'ab'));
+    # for i in bHome:
+    #     for a in bScore:
+    #         for b in bAway:
+    #             fileWriter.writerow([i] + [a] + [b]);
+    for i in range(len(bHome)):
+        fileWriter.writerow([bHome[i]] + [bScore[i]] + [bAway[i]]);
+
+
+page = 20120815;
+writeCSV_init();
+battleDate = [];
+battleDate.append('Friday, August 17, 2012');
+while page <= 20130519:
+    page = str(page);
+    response = urllib2.urlopen('http://espnfc.com/results/_/date/'+page+'/league/eng.1/english-premier-league?cc=5901');
+    html = response.read();
+    soup = BeautifulSoup(html);
+    battleHome = [];
+    battleScore = [];
+    battleLinks = [];
+    battleAway = [];
+    #print soup.prettify();
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    for a in soup.find_all('table'):
+        #print a, "\n";
+        print "Date: ", extractDate(a);
+        if existDate(battleDate, extractDate(a)) == "true":
+            print "same\n";
+        else: 
+            battleDate.append(extractDate(a));
+            battleHome = extractHome(a);
+            #for i in range(len(battleHome)):
+               #print "Home: ", battleHome[i];
+            battleScore = extractScore(a);
+            #for i in range(len(battleScore)):
+            #    print "Score: ", battleScore[i];
+            battleLinks = extractLinks(a);
+            #for i in range(len(battleLinks)):
+            #    print "Link: ", battleLinks[i];
+            battleAway = extractAway(a);
+            #for i in range(len(battleAway)):
+            #    print "Away: ", battleAway[i];
+            writeCSV(battleDate, battleHome, battleScore, battleAway);
+    page = int(page);
+    page = page + 15;
--- start.py
+++ start.py
@@ -1,26 +1,85 @@
 import urllib2;
 import re;
-response = urllib2.urlopen('http://espnfc.com/results/_/date/20120818/league/eng.1/english-premier-league?cc=5901');
+response = urllib2.urlopen('http://espnfc.com/results/_/date/20120820/league/eng.1/english-premier-league?cc=5901');
 html = response.read();
-#print html
-findDate = re.findall('<tr class="stathead"><td colspan="7">(.*)</td></tr>', html);
-if (findDate):
-    #print "start date: ", findDate;
-    #fDate = findDate.group();
-    print "start date: ", findDate;
-findHome = re.findall('<td align="right"><a href=.*>(.*)</a></td>', html);
+html1 = html;
+p=re.compile("\s+")
+html=re.sub(p,'',html)
+print html;
+print html1;
+print "~~~~~~~~~~~~~~~~~";
+
+def extractTable(wholeHtml):
+    findData = re.findall('<tableclass="tablehead"cellpadding="3"cellspacing="1">.*?</table></div></div>', html);
+    return findData;
+
+table = extractTable(html);
+print table, "\n";
+
+def extractDate(stringContent):
+    findDate = re.findall('<trclass="stathead"><tdcolspan="7">(.*)</td></tr><trclass="colhead">', stringContent);
+    if (findDate):
+        return findDate;
+    else:
+        return 'nothing';
+
+def extractHome(stringContent):
+    findHome = re.findall('<tdalign="right"><ahref=.*>(.*)</a></td><tdalign="center">', stringContent);
+    print findHome;
+    return findHome;
+
+def extractScore(stringContent):
+    findScore = re.findall('<tdalign="center"><ahref=.*>(.-.)</a></td><tdalign="left">', stringContent);
+    return findScore;
+
+def extractAway(stringContent):
+    findAway = re.findall('<tdalign="left"><ahref=.*>(.*?)</a></td>', stringContent);
+    return findAway;
+
+for a in range(len(table)):
+    print table[a], "1\n";
+    Date = extractDate(table[a]);
+    print "Date: ", Date, "1\n";
+    Home = extractHome(table[a]);
+    #print "Home: ", Home;
+    print "1\n";
+    Score = extractScore(table[a]);
+    print "Score: ", Score;
+    print "1\n";
+    Away = extractAway(table[a]);
+    print "Away: ", Away;
+    print "1\n";
+
+
+
+# pattern='<tableclass="tablehead"cellpadding="3"cellspacing="1">.*</table></div></div>';
+ 
+# for match in re.finditer(pattern,html):
+#     s=match.start()
+#     e=match.end()
+#     print "Found %s at %d,%d" % (text[s:e],s,e)
+
+
+
+
+# findDate = re.findall('<tableclass="tablehead"cellpadding="3"cellspacing="1">.*?</table></div></div>', html);
+# if (findDate):
+#     #print "start date: ", findDate;
+#     #fDate = findDate.group();
+#     print "start date: ", findDate;
+findHome = re.findall('<td align="right"><a href=.*>(.*)</a></td>', html1);
 if (findHome):
     #print "start date: ", findHome;
     #fHome = findHome.group();
-    print "home: ", findHome;
-findScore = re.findall('<td align="center"><a href=.*>(.*)</a></td>', html);
-if (findScore):
-    #print "start date: ", findScore;
-    #fScore = findScore.group();
-    print "  ", findScore, "  ";
-findAway = re.findall('<td align="left"><a href=.*>(.*)</a></td>', html);
-if (findAway):
-    #print "start date: ", findAway;
-    #fAway = findAway.group();
-    print "away: ", findAway;
-#print "home: ", findHome, "  ", findScore, "  ", "away:", findAway;
+    print "11111home: ", findHome;
+# findScore = re.findall('<tdalign="center"><ahref=.*>(.*)</a></td>', html);
+# if (findScore):
+#     #print "start date: ", findScore;
+#     #fScore = findScore.group();
+#     print "  ", findScore, "  ";
+# findAway = re.findall('<tdalign="left"><ahref=.*>(.*)</a></td>', html);
+# if (findAway):
+#     #print "start date: ", findAway;
+#     #fAway = findAway.group();
+#     print "away: ", findAway;
+# #print "home: ", findHome, "  ", findScore, "  ", "away:", findAway;
